0528cda13e2c34c7e0faa69631b8959b53d30601
krismutalapova <kris.mutalapova@pm.me>
2025-07-14 12:24:12 +0200

    fix: update Django settings for testing compatibility

--- skillswap/settings.py
+++ skillswap/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = "replace-this-with-a-secure-key"
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["testserver", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
